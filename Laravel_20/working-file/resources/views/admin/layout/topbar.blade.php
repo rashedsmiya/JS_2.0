6a1268170fd4a875fb2180d3bcddf37ae0744530
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-06-30 21:51:14 +0600

    add

--- Laravel_20/working-file/resources/views/admin/layout/topbar.blade.php
+++ Laravel_20/working-file/resources/views/admin/layout/topbar.blade.php
@@ -185,7 +185,7 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="{{asset('backend/assets/images/avatars/avatar-1.png')}}" class="msg-avatar" alt="user avatar">
+                                            <img src="{{ asset('backend/assets/images/avatars/avatar-1.png') }}" class="msg-avatar" alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Daisy Anderson <span class="msg-time float-end">5 sec
@@ -197,7 +197,7 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="{{asset('backend/assets/images/avatars/avatar-2.png')}}" class="msg-avatar" alt="user avatar">
+                                            <img src="{{ asset('backend/assets/images/avatars/avatar-2.png') }}" class="msg-avatar" alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Althea Cabardo <span class="msg-time float-end">14
@@ -209,7 +209,7 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="{{asset('backend/assets/images/avatars/avatar-3.png')}}" class="msg-avatar" alt="user avatar">
+                                            <img src="{{ asset('backend/assets/images/avatars/avatar-3.png') }}" class="msg-avatar" alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Oscar Garner <span class="msg-time float-end">8 min
@@ -221,7 +221,7 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="{{asset('backend/assets/images/avatars/avatar-4.png')}}" class="msg-avatar" alt="user avatar">
+                                            <img src="{{ asset('backend/assets/images/avatars/avatar-4.png') }}" class="msg-avatar" alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Katherine Pechon <span class="msg-time float-end">15
@@ -233,7 +233,7 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="{{asset('backend/assets/images/avatars/avatar-5.png')}}" class="msg-avatar" alt="user avatar">
+                                            <img src="{{ asset('backend/assets/images/avatars/avatar-5.png') }}" class="msg-avatar" alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Amelia Doe <span class="msg-time float-end">22 min
@@ -245,7 +245,7 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="{{asset('backend/assets/images/avatars/avatar-6.png')}}" class="msg-avatar" alt="user avatar">
+                                            <img src="{{ asset('backend/assets/images/avatars/avatar-6.png') }}" class="msg-avatar" alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Cristina Jhons <span class="msg-time float-end">2 hrs
@@ -257,7 +257,7 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="{{asset('backend/assets/images/avatars/avatar-7.png')}}" class="msg-avatar" alt="user avatar">
+                                            <img src="{{ asset('backend/assets/images/avatars/avatar-7.png') }}" class="msg-avatar" alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">James Caviness <span class="msg-time float-end">4 hrs
@@ -269,7 +269,7 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="{{asset('backend/assets/images/avatars/avatar-8.png')}}" class="msg-avatar" alt="user avatar">
+                                            <img src="{{ asset('backend/assets/images/avatars/avatar-8.png') }}" class="msg-avatar" alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Peter Costanzo <span class="msg-time float-end">6 hrs
@@ -281,7 +281,7 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="{{asset('backend/assets/images/avatars/avatar-9.png')}}" class="msg-avatar" alt="user avatar">
+                                            <img src="{{ asset('backend/assets/images/avatars/avatar-9.png') }}" class="msg-avatar" alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">David Buckley <span class="msg-time float-end">2 hrs
@@ -293,7 +293,7 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="{{asset('backend/assets/images/avatars/avatar-10.png')}}" class="msg-avatar" alt="user avatar">
+                                            <img src="{{ asset('backend/assets/images/avatars/avatar-10.png') }}" class="msg-avatar" alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Thomas Wheeler <span class="msg-time float-end">2 days
@@ -305,7 +305,7 @@
                                 <a class="dropdown-item" href="javascript:;">
                                     <div class="d-flex align-items-center">
                                         <div class="user-online">
-                                            <img src="{{asset('backend/assets/images/avatars/avatar-11.png')}}" class="msg-avatar" alt="user avatar">
+                                            <img src="{{ asset('backend/assets/images/avatars/avatar-11.png') }}" class="msg-avatar" alt="user avatar">
                                         </div>
                                         <div class="flex-grow-1">
                                             <h6 class="msg-name">Johnny Seitz <span class="msg-time float-end">5 days
@@ -326,7 +326,7 @@
                 <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="{{ asset('backend/assets/images/avatars/avatar-2.png') }}" class="user-img" alt="user avatar">
                     <div class="user-info ps-3">
-                        <p class="user-name mb-0">Pauline Seitz</p>
+                        <p class="user-name mb-0">{{Auth::user()->name }}</p>
                         <p class="designattion mb-0">Web Designer</p>
                     </div>
                 </a>
--- Laravel_20/working-file/routes/web.php
+++ Laravel_20/working-file/routes/web.php
@@ -3,13 +3,25 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 Route::get('/', function () {
+    // return view('welcome');  
     return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
-    // return view('dashboard');  
-    return view('admin.index');
+    // return view('dashboard');
+    return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
